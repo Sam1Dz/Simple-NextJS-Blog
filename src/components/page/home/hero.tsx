ecfc4123ae07f3cded386c40b9a0d310194bd531
Pratama Dimas <dimasgamers01@gmail.com>
2023-04-10 00:28:10 +0700

    fixing Hero Components (regarding responsive)

--- src/components/page/home/hero.tsx
+++ src/components/page/home/hero.tsx
@@ -1,56 +1,55 @@
 import Image from "next/image";
+import { Box, useMediaQuery } from "@mui/material";
 
-import { Avatar, Typography } from "@mui/material";
-
-import styles from "@/styles/hero.module.css";
+import HeroContentComponent from "./hero-content";
 
 function HeroComponent() {
+  const heroHeightUnder375 = useMediaQuery("(min-height:300px)");
+
   return (
-    <section className={styles.hero}>
-      <div className={styles.acrylicEffect}>
+    <Box
+      component="section"
+      sx={{
+        position: "relative",
+        width: "100%",
+        height: heroHeightUnder375
+          ? "calc(100vh - 56px)"
+          : "calc(100vh + 56px)",
+        overflow: "hidden",
+      }}
+    >
+      <Box
+        component="div"
+        sx={{
+          position: "inherit",
+          height: "100%",
+          boxShadow: "inset 0 0 2000px rgba(255, 255, 255, 0.5)",
+          filter: "blur(15px) brightness(0.25)",
+        }}
+      >
         <Image
           fill
           src="/images/site/hero-background.jpg"
           alt="Hero Background"
           priority
-          className={styles.backgoroundImage}
+          style={{ objectFit: "cover", zIndex: -1 }}
         />
-      </div>
-      <div className={styles.heroProfile}>
-        <Avatar
-          sx={{
-            width: 300,
-            height: 300,
-            boxShadow: "0 1px 8px 16px rgba(0, 0, 0, 0.2)",
-            overflow: "hidden",
-            m: "auto",
-          }}
-        >
-          <Image
-            src="/images/site/sam1dz-pp.jpg"
-            alt="Sam1Dz Profile Photo"
-            width={300}
-            height={300}
-          />
-        </Avatar>
-
-        <Typography
-          variant="h2"
-          component="h1"
-          gutterBottom
-          sx={{ mt: "1rem" }}
-        >
-          Hi, i&apos;m &quot;Sam1Dz&quot;
-        </Typography>
-        <Typography variant="h5" component="p">
-          This is just an example of a Web Blog created by NextJS
-        </Typography>
-        <Typography variant="overline" component="p">
-          &quot;Enhanced version from NextJS course by Maximilian
-          Schwarzmüller&quot;
-        </Typography>
-      </div>
-    </section>
+      </Box>
+      <Box
+        component="div"
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+          width: "100%",
+          p: 1,
+        }}
+      >
+        <HeroContentComponent />
+      </Box>
+    </Box>
   );
 }
 
